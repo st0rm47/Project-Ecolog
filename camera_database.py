2f6187245f1914b1ff3eaf584ffee2bd88e06a3c
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2024-02-15 13:53:49 +0545

    Camera to Azure Completed

--- camera_database.py
+++ camera_database.py
@@ -3,12 +3,17 @@ import numpy as np
 import time
 from azure.storage.blob import BlobServiceClient
 
-#Connection String and Container Name
+#Connection String and Container Name with the help of BlobServiceClient
 connection_str = "DefaultEndpointsProtocol=https;AccountName=ecologstorage;AccountKey=GcQyJX5gaXrgMV4zaIeIWGuuoubKuRp2E7vMmQl4kFP5qnyun1MfikOMVlclmICiaJK4r5+NdGe6+AStz89CFQ==;EndpointSuffix=core.windows.net"
 container_name = "images"
 
 #Creating a blob service client
 blob_service_client = BlobServiceClient.from_connection_string(connection_str)
+
+#Creating a container client
+container_client = blob_service_client.get_container_client(container_name)
+
+
 
 #Open the camera
 camera = cv2.VideoCapture(0)
@@ -32,10 +37,13 @@ cv2.imwrite(save_path, save_image)
 
 #Upload the image to the azure storage
 local_file_path = "captured_frame.jpg"
-blob_client = blob_service_client.get_blob_client(container=container_name, blob="updated(1).jpg")
 
-camera.release()
-cv2.destroyAllWindows()
-with open(local_file_path, "rb") as data:
+
+blob_name = "Forest.jpg"
+with open(save_path, "rb") as data:
+    blob_client = container_client.get_blob_client(blob_name)
     blob_client.upload_blob(data)
     print("Image uploaded to the azure storage")
+    
+camera.release()
+cv2.destroyAllWindows()
