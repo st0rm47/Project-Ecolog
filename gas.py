36191cff14aafe0e1258e2cc09586b740626677b
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2024-02-15 21:19:10 +0545

    Gas Worked

--- gas.py
+++ gas.py
@@ -2,7 +2,7 @@
 import RPI.GPIO as GPIO
 import time
 
-#Setting up GPIO
+#Setting up GPIO v   
 GPIO.setmode(GPIO.BCM)
 
 #Setting up the gas sensor pin
@@ -14,13 +14,13 @@ GPIO.setup(DOUT_PIN, GPIO.IN)
 try:
     while True:
         # Reading the state of the digital output pin
-        digital_value = GPIO.input(DOUT_PIN)
-        
+        digital_value = GPIO.input(DOUT_PIN) 
         #Threshold Value
         threshold = 200
 
         if digital_value == GPIO.LOW or digital_value > threshold:
             print("Smoke Detected")
+            
         else:
             print("No Smoke Detected")       
         time.sleep(1)
