9b3d3346124a16e1453c18b5d5a777b7717cde70
Firojpaudel <firojpaudel@gmail.com>
2024-02-16 23:28:29 +0545

    sdadas

--- Project/main.py
+++ Project/main.py
@@ -161,7 +161,7 @@ def send_signal():
 #     return 'Popup opened successfully', 200
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
 
 
