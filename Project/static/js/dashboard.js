bb2ee06f7f06bd96314260e2b37fb33419d1a030
Firojpaudel <firojpaudel@gmail.com>
2024-02-21 21:26:35 +0545

    Converted from light to dark theme - Looking more aesthetic :)

--- Project/static/js/dashboard.js
+++ Project/static/js/dashboard.js
@@ -1,6 +1,9 @@
 function redirectToAnotherPage() {
     window.location.href = '/live';
 }
+function dashboard() {
+    window.location.href = '/dashboard';
+}
 function logout() {
     window.location.href = '/logout';
 }
